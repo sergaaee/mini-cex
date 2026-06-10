ad6e9d7b4a6794678690210a63aaf56e9e406dd6
d10658 <sergoiwj@duck.com>
2026-06-10 13:29:56 +0300

    remove aster

--- aggregator/src/price_aggregator.rs
+++ aggregator/src/price_aggregator.rs
@@ -30,10 +30,10 @@ impl Aggregator {
         tokio::spawn(ws::run_hibachi(quotes_clone, publisher_clone));
         info!("Hibachi started!");
 
-        let quotes_clone = Arc::clone(&quotes);
-        let publisher_clone = publisher.clone();
-        tokio::spawn(ws::run_aster(quotes_clone, publisher_clone));
-        info!("Aster started!");
+        // let quotes_clone = Arc::clone(&quotes);
+        // let publisher_clone = publisher.clone();
+        // tokio::spawn(ws::run_aster(quotes_clone, publisher_clone));
+        // info!("Aster started!");
 
         // let quotes_clone = Arc::clone(&quotes);
         // let publisher_clone = publisher.clone();
